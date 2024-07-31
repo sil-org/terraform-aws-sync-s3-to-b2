32a17419f693869566041be8ab99f59fc9892050
Dale Newby <dale_newby@sil.org>
2024-07-31 08:54:21 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# template-public
-general template for public repositories
+# sync-s3-to-b2 - Synchronize the content of an AWS S3 bucket on a Backblaze B2 bucket
+
+This Terraform module replicates the changes in an AWS S3 bucket onto a Backblaze B2 bucket resulting in the B2 bucket content being identical to the S3 bucket content.
+
+## What this does
+
+* Creates an IAM user with permissions to read the S3 bucket
+* Creates an ECS task definition
+* Schedules that task to run the `sync-s3-to-b2` Docker image
+
+## Required Inputs
+
+* `app_name` - Name of the application
+* `app_env` - Short name of the environment, e.g., prod, stg, etc.
+* `b2_application_key_id` - Backblaze Application Key ID
+* `b2_application_key` - Backblaze Application Key secret
+* `b2_bucket` - Name of the Backblaze B2 bucket
+* `b2_path` - Path within the Backblaze B2 bucket where files will be stored
+* `cpu` - Amount of CPU to allocate to the task
+* `ecs_cluster_id` - ID of the ECS Cluster where this scheduled task should run
+* `log_group_name` - CloudWatch Log Group to write logs to
+* `memory` - Amount of memory to allocate to the task
+* `schedule` - S3-to-B2 backup schedule, e.g., `cron(10 2 * * ? *)`
+* `s3_bucket_name` - Name of the S3 bucket to sync to B2, e.g., `my-bucket`
+* `s3_path` - Path to be backed up within the AWS S3 bucket
+
+## Optional Inputs
+
+* `rclone_arguments` - Extra arguments to pass to the `rclone` command; default is ""
+
+## Outputs
+
+_none_
+
+## Example Usage
+
+```hcl
+module "sync_s3_to_b2" {
+  source = "./modules/sync-s3-to-b2"
+
+  app_name              = var.app_name
+  app_env               = var.app_env
+  b2_application_key_id = var.b2_application_key_id
+  b2_application_key    = var.b2_application_key
+  b2_bucket             = var.b2_bucket_name
+  b2_path               = var.b2_path
+  cpu                   = var.backup_cpu
+  ecs_cluster_id        = data.terraform_remote_state.common.outputs.ecs_cluster_id
+  log_group_name        = aws_cloudwatch_log_group.my_loggroup.name
+  memory                = var.backup_memory
+  rclone_arguments      = "--verbose"
+  schedule              = "cron(10 2 * * ? *)"
+  s3_bucket_name        = var.s3_bucket_name
+  s3_path               = var.s3_path
+}
+```
---
+++ main.tf
@@ -0,0 +1,138 @@
+
+/*
+ * Create user for getting files from the bucket
+ */
+resource "aws_iam_user" "clone" {
+  name = "s3-clone-${local.app_name_and_env}"
+}
+
+resource "aws_iam_access_key" "clone" {
+  user = aws_iam_user.clone.name
+}
+
+resource "aws_iam_user_policy" "clone" {
+  name = "S3-Clone-Backup"
+  user = aws_iam_user.clone.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+        ]
+        Resource = [
+          "${data.aws_s3_bucket.this.arn}/*",
+          data.aws_s3_bucket.this.arn
+        ]
+      }
+    ]
+  })
+}
+
+data "aws_region" "current" {}
+
+data "aws_s3_bucket" "this" {
+  bucket = var.s3_bucket_name
+}
+
+/*
+ * Create ECS service container definition
+ */
+locals {
+  app_name_and_env = "${var.app_name}-${var.app_env}"
+  aws_region       = data.aws_region.current.name
+
+  task_def_clone = templatefile("${path.module}/task-def-copys3b2.json", {
+    app_name              = var.app_name
+    aws_access_key        = aws_iam_access_key.clone.id
+    aws_secret_key        = aws_iam_access_key.clone.secret
+    aws_region            = local.aws_region
+    b2_application_key_id = var.b2_application_key_id
+    b2_application_key    = var.b2_application_key
+    s3_bucket             = var.s3_bucket_name
+    s3_path               = var.s3_path
+    b2_bucket             = var.b2_bucket
+    b2_path               = var.b2_path
+    rclone_arguments      = var.rclone_arguments
+    log_group_name        = var.log_group_name
+    cpu                   = var.cpu
+    memory                = var.memory
+  })
+}
+
+/*
+ * Create role for scheduled running of rclone task definition.
+ */
+resource "aws_iam_role" "rclone_event" {
+  name = "rclone_event-${local.app_name_and_env}-s3copy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "rclone_event_run_task_with_any_role" {
+  name = "rclone_event_run_task_with_any_role"
+  role = aws_iam_role.rclone_event.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = concat([
+      {
+        Effect   = "Allow"
+        Action   = "iam:PassRole"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "ecs:RunTask"
+        Resource = "${aws_ecs_task_definition.clone_cron_td.arn_without_revision}:*"
+      }
+    ])
+  })
+}
+
+/*
+ * Create bucket clone task definition
+ */
+resource "aws_ecs_task_definition" "clone_cron_td" {
+  family                = "${var.app_name}-clone-${var.app_env}"
+  container_definitions = local.task_def_clone
+  network_mode          = "bridge"
+}
+
+/*
+ * CloudWatch configuration to start scheduled backup of S3 using rclone.
+ */
+resource "aws_cloudwatch_event_rule" "s3_clone_event_rule" {
+  name        = "${local.app_name_and_env}-s3clone"
+  description = "Start scheduled backup of S3 using rclone"
+
+  schedule_expression = var.schedule
+}
+
+resource "aws_cloudwatch_event_target" "clone_event_target" {
+  target_id = "${local.app_name_and_env}-s3clone"
+  rule      = aws_cloudwatch_event_rule.s3_clone_event_rule.name
+  arn       = var.ecs_cluster_id
+  role_arn  = aws_iam_role.rclone_event.arn
+
+  ecs_target {
+    task_count          = 1
+    launch_type         = "EC2"
+    task_definition_arn = aws_ecs_task_definition.clone_cron_td.arn
+  }
+}
